d39aeab6d5acbbdb55fc5d70f46bb76c5fa6bfcb
Chris Arderne <chris@rdrn.me>
2021-07-26 13:19:45 +0100

    Modify prep-historic script

--- prep-historic.py
+++ prep-historic.py
@@ -9,6 +9,7 @@ for f in Path("data").glob("*_historic.csv"):
     name = f.stem.split("_")[0]
     df = pd.read_csv(f).dropna()
     df.columns = ["x", "y"]
+    df = df.loc[df.x >= "2017-01-01"]
     res[name] = df.to_dict(orient="records")
 
 print(f"const historic = {res}")
