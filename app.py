00feba2c9b4d7d581a694295e31cda766b71fc51
Chris Arderne <chris@rdrn.me>
2022-03-08 15:49:25 +0000

    Update API and BQ table refs

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ bqclient = bigquery.Client(
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 REDIS_PW = os.environ.get("REDIS_PW", None)
+CACHE_BUST = os.environ.get("REDIS_CACHE_BUST", False)
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PW)
 redis_expiry = 60 * 60 * 24 * 2  # 2 days
 
@@ -28,12 +29,15 @@ def cache(prefix):
     def decorator_cache(func):
         @functools.wraps(func)
         def wrapper_cache(*args, **kwargs):
-            suffix = kwargs.get("reservoir", "")
-            key = f"{prefix}.{suffix}"
-            if data := r.get(key):
-                return json.loads(data)
-            data = func(*args, **kwargs)
-            r.set(key, json.dumps(data), ex=redis_expiry)
+            if CACHE_BUST:
+                data = func(*args, **kwargs)
+            else:
+                suffix = kwargs.get("reservoir", "")
+                key = f"{prefix}.{suffix}"
+                if data := r.get(key):
+                    return json.loads(data)
+                data = func(*args, **kwargs)
+                r.set(key, json.dumps(data), ex=redis_expiry)
             return data
 
         return wrapper_cache
@@ -52,7 +56,7 @@ def get_reservoir_list():
 
 
 @cache("forecast")
-def get_forecast(*, reservoir, date):
+def get_prediction(*, reservoir, date):
     reservoir = reservoir.split(" ")[0]  # naive prevent any injection!
     query = f"""
     SELECT forecast FROM `oxeo-main.wave2web.prediction`
@@ -77,11 +81,11 @@ def get_forecast(*, reservoir, date):
 def get_historic(*, reservoir, date):
     start_date = date - dt.timedelta(days=365)
     query = f"""
-    SELECT date, volume, precip FROM `oxeo-main.wave2web.historic`
-    WHERE `reservoir` = "{reservoir}"
-    AND `date` >= "{start_date.date().isoformat()}"
-    AND `date` < "{date.date().isoformat()}"
-    ORDER BY `date`
+    SELECT DATETIME, WATER_VOLUME * 1000 FROM `oxeo-main.wave2web.reservoir-data`
+    WHERE `RESERVOIR_NAME` = "{reservoir}"
+    AND `DATETIME` >= "{start_date.date().isoformat()}"
+    AND `DATETIME` < "{date.date().isoformat()}"
+    ORDER BY `DATETIME`
     """
     job = bqclient.query(query)
     data = (row.values() for row in job)
@@ -89,7 +93,7 @@ def get_historic(*, reservoir, date):
         {
             "x": row[0].isoformat(),
             "volume": row[1],
-            "precip": row[2],
+            "precip": 0,
         }
         for row in data
     ]
@@ -100,33 +104,17 @@ def get_historic(*, reservoir, date):
 def get_levels():
     query = """
     SELECT
-        historic.reservoir,
-        historic.volume,
-        prediction.forecast[
-            ORDINAL(ARRAY_LENGTH(prediction.forecast))
-        ] as forecast
-
+        historic.RESERVOIR_NAME,
+        historic.WATER_VOLUME * 1000,
     FROM (
-        SELECT t1.reservoir, t1.volume
-        FROM `oxeo-main.wave2web.historic` t1
-        WHERE t1.date = (
-            SELECT MAX(t2.date)
-            FROM `oxeo-main.wave2web.historic` t2
-            WHERE t2.reservoir = t1.reservoir
+        SELECT t1.RESERVOIR_NAME, t1.WATER_VOLUME
+        FROM `oxeo-main.wave2web.reservoir-data` t1
+        WHERE t1.DATETIME = (
+            SELECT MAX(t2.DATETIME)
+            FROM `oxeo-main.wave2web.reservoir-data` t2
+            WHERE t2.RESERVOIR_NAME = t1.RESERVOIR_NAME
         )
     ) AS historic
-
-    JOIN (
-        SELECT t1.reservoir, t1.forecast
-        FROM `oxeo-main.wave2web.prediction` t1
-        WHERE t1.date = (
-            SELECT MAX(t2.date)
-            FROM `oxeo-main.wave2web.prediction` t2
-            WHERE t2.reservoir = t1.reservoir
-        )
-    ) AS prediction
-
-    ON historic.reservoir = prediction.reservoir
     """
     job = bqclient.query(query)
     data = [dict(row) for row in job]
@@ -157,9 +145,9 @@ def levels():
     return jsonify(levels)
 
 
-@app.route("/api/forecasts")
+@app.route("/api/predictions")
 @auth.login_required
-def forecasts():
+def predictions():
     date = request.args.get("date")
 
     try:
@@ -170,7 +158,7 @@ def forecasts():
     try:
         reservoirs = get_reservoir_list()
         data = [
-            {"reservoir": res, "forecast": get_forecast(reservoir=res, date=date)}
+            {"reservoir": res, "prediction": get_prediction(reservoir=res, date=date)}
             for res in reservoirs
         ]
         return jsonify(data)
@@ -179,9 +167,9 @@ def forecasts():
         return jsonify({"Error": f"bad request: {e}"})
 
 
-@app.route("/api/forecast")
+@app.route("/api/prediction")
 @auth.login_required
-def forecast():
+def prediction():
     reservoir = request.args.get("reservoir")
     date = request.args.get("date")
 
@@ -191,7 +179,7 @@ def forecast():
         return jsonify({"error": f"specify a date as YYYY-MM-DD: {e}"})
 
     try:
-        return jsonify(get_forecast(reservoir=reservoir, date=date))
+        return jsonify(get_prediction(reservoir=reservoir, date=date))
     except Exception as e:
         print("Error!", e)
         return jsonify({"Error": f"bad request: {e}"})
